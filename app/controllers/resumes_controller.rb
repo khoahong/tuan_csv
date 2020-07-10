b22d9e1e3b768deb3ef068510c42e6c062641b76
khoahong <hongakhoa@gmail.com>
2020-07-10 11:25:00 +0800

    Intial commit

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,34 @@
+class ResumesController < ApplicationController
+   def index
+      @resumes = Resume.all
+   end
+   
+   def new
+      @resume = Resume.new
+   end
+   
+   def create
+      @resume = Resume.new(resume_params)
+      
+      contents = @resume.attachment.read
+
+      if @resume.save
+         redirect_to resumes_path, notice: "Successfully uploaded."
+      else
+         render "new"
+      end
+      
+   end
+   
+   def destroy
+      @resume = Resume.find(params[:id])
+      @resume.destroy
+      redirect_to resumes_path, notice:  "Successfully deleted."
+   end
+   
+   private
+      def resume_params
+      params.require(:resume).permit(:name, :attachment)
+   end
+   
+end
